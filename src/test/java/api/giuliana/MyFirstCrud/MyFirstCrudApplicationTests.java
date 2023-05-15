a1c1eb5b9fe2f08b72417537d032a0166aafd387
Giulianna Grego <giuliannagc@outlook.com>
2023-05-14 22:22:18 -0300

    first crud project

---
+++ src/test/java/api/giuliana/MyFirstCrud/MyFirstCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package api.giuliana.MyFirstCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
